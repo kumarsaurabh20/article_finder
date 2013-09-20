bc1bb90b62c9974f45347139bf1b2820ed6eae82
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2013-09-20 17:43:14 +0200

    update

---
+++ init.rb
@@ -0,0 +1,12 @@
+##Article Finder App###
+
+##Launch this file from the command line to start the application and get started###
+
+APP_ROOT = File.dirname(__FILE__)
+
+#require "#{APP_ROOT}/lib/guide"
+#require File.join(APP_ROOT, 'lib', 'guide.rb')
+#require File.join(APP_ROOT, 'lib', 'guide')
+
+$:.unshift(File.join(APP_ROOT, 'lib'))
+require 'guide'
